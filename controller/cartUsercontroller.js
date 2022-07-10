9439d29ed4aa45dbfb844a29bc3484cdd984d32c
Trungdev <trungdev1611@gmail.com>
2022-07-10 16:19:29 +0700

    Lay du lieu gio hang tu server

--- controller/cartUsercontroller.js
+++ controller/cartUsercontroller.js
@@ -1,19 +1,38 @@
+
 const InfoCart = require('./../models/InfoCart')
-const getCartUser = (req, res) => {
+const getCartUser = async (req, res) => {
+    let data = await InfoCart.findAll({
+        where: {
+            //req.userId la Id cua nguoi dung duoc gui trong token
+            UserId: req.userId
+        }
+    })
+    if (data && data.length === 0) {
+        return res.status(422).json({
+            message: "Ban chua co gi trong gio hang",
+            data: data
+        })
+    }
+
     res.json({
-        data: 'getCartUser success'
+        message: "Lay du lieu gio hang tu server ve thanh cong",
+        data: data
     })
 }
 
 const postproductCart = async (req, res) => {
     const cartdata = req.body
+
     if (!cartdata) {
         return res.json({
             error: "khong co body"
         })
     }
+    //neu ton tai cartdata
+    cartdata.UserId = req.userId
     // tim trong db xem da co ton tai viec nguoi dung do  da mua san pham do chua. Neu 
     // co thi cap nhat so luong, neu chua thi them moi
+
     let checkExitsUserProduct = await InfoCart.findOne({
         where: {
             idproduct: req.body.idproduct,
