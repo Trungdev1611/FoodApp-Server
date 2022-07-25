cdb735f58e94164b30b1b3370d83c8a154aafed2
Trungdev <trungdev1611@gmail.com>
2022-07-25 20:53:46 +0700

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cors = require('cors');
 const sequelize = require('./ConnectDB');
 require('dotenv').config()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
 app.use(express.json())
 
 //Association
